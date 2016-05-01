f3dfb9c415755e3b3dfb7d1267f0875f28d4534d
mthology <matt@attribyte.com>
2016-05-01 11:10:28 -0500

    Remove publisher stage script

--- stage_publisher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cp pubsubhub-publisher-1.0.0.pom dist_publisher/lib
-cd dist_publisher/lib
-gpg -ab pubsubhub-publisher-1.0.0.pom
-gpg -ab pubsubhub-publisher-1.0.0.jar
-gpg -ab pubsubhub-publisher-1.0.0-sources.jar
-gpg -ab pubsubhub-publisher-1.0.0-javadoc.jar
-jar -cvf ../bundle.jar *
-
